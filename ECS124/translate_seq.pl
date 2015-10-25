1f86dc9c937b909ae36b4832c7be3f131d013def
apham <apham@ucdavis.edu>
2015-10-25 04:19:49 -0700

    removed redundant files

--- ECS124/translate_seq.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-$sequence = 'SEETQMRLQLKRKLQRNRTSFTQEQIEALEKEFERTHYPDVFARERL';
-print "$sequence\n";
-
-$sequence =~ tr/ACGPSTWY/A/;
-$sequence =~ tr/RNDQEHK/E/;
-$sequence =~ tr/ILMFV/I/;
-
-print "$sequence\n";
--- ECS124/upperToLower.pl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/perl
-
-$sequence = 'VRNrIAEelslrrFMVALILdIKrTPgNKPriaemICDIDtYIvEa';
-print "$sequence\n";
-
-$sequence =~ tr/A-Za-z/a-zA-Z/;
-print "$sequence\n";
